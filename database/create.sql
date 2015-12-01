4a8875552a1cadd8d59fecc53d772259e0b763c2
Katya <kpfomenko@gmail.com>
2015-11-30 20:03:21 -0800

    Adding create and insert sql files for database to be created on AWS

---
+++ database/create.sql
@@ -0,0 +1,11 @@
+-- create.sql by Katya Fomenko and Deanna Hartsook
+DROP DATABASE IF EXISTS project;
+CREATE DATABASE project;
+USE project;
+
+CREATE TABLE Employees (
+	id INTEGER,
+	age INTEGER,
+	salary TEXT NOT NULL,
+	PRIMARY KEY(id)
+);
---
+++ database/insert.sql
@@ -0,0 +1,29 @@
+-- insert.sql by Katya Fomenko and Deanna Hartsook
+USE project;
+
+INSERT INTO Employees(id, age, salary)
+	VALUES(11, 25, 1000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(31, 25, 2000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(32, 25, 3000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(34, 25, 2200);
+INSERT INTO Employees(id, age, salary)
+	VALUES(200, 45, 10000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(123, 35, 2100);
+INSERT INTO Employees(id, age, salary)
+	VALUES(42, 14, 1235);
+INSERT INTO Employees(id, age, salary)
+	VALUES(53, 60, 5245);
+INSERT INTO Employees(id, age, salary)
+	VALUES(15, 55, 5235);
+INSERT INTO Employees(id, age, salary)
+	VALUES(16, 35, 1035);
+INSERT INTO Employees(id, age, salary)
+	VALUES(17, 35, 20000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(2, 42, 200000);
+INSERT INTO Employees(id, age, salary)
+	VALUES(0, 33, 10000);
