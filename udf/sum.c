14d0c31546b5ad8ca11346b88a570c984e7478c7
Katya <kpfomenko@gmail.com>
2015-11-30 21:13:49 -0800

    Updated makefile and sum.c/sum.h files -- working on UDF

--- udf/sum.c
+++ udf/sum.c
@@ -1,18 +1,36 @@
 //
 // Created by Deanna Hartsook on 11/29/15.
 //
-
 #include "sum.h"
+#include <string.h>
 
-long long MySum(UDF_INIT *initid, UDF_ARGS *args,
-          char *is_null, char *error) {
-    return 10;
+my_bool mySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message){
+	if(!(args->arg_count == 1)) {
+		strcpy(message, "Expected one argument --> for calculating sum ");
+		return 1;
+	}
+	args->arg_type[0] = REAL_RESULT;
+	longlong* sum = (longlong *)malloc(sizeof(longlong));
+	*sum = 0;
+
+	initid->ptr = (char *)sum;
+
+	return 0;
 }
 
-my_bool MySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
-    return 0;
+void mySum_deinit(UDF_INIT *initid __attribute__((unused))){
+	free(initid->ptr);
 }
 
-void MySum_deinit(UDF_INIT *initid) {
-
+longlong mySum(UDF_INIT* initid, UDF_ARGS* args __attribute__((unused)), char* is_null __attribute__((unused)), char* error __attribute__((unused))){
+    return  *((longlong *) initid->ptr);
 }
+
+void mySum_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error){
+	*((longlong *) initid->ptr) = *((longlong *) initid->ptr) + *((longlong *) args->args[0]);	
+}
+
+void mySum_clear(UDF_INIT *initid, char *is_null, char *error){
+	*((longlong *) initid->ptr) = 0;
+}
+
--- udf/sum.h
+++ udf/sum.h
@@ -6,12 +6,12 @@
 #define CS174FINALPROJECT_SUM_H
 
 #include <mysql.h>
+#include <my_global.h>
 
-long long MySum(UDF_INIT *initid, UDF_ARGS *args,
-              char *is_null, char *error);
-
-my_bool MySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
-
-void MySum_deinit(UDF_INIT *initid);
+my_bool mySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+void mySum_deinit(UDF_INIT *initid __attribute__((unused)));
+longlong  mySum(UDF_INIT* initid, UDF_ARGS* args __attribute__((unused)), char* is_null __attribute__((unused)), char* error __attribute__((unused)));
+void mySum_clear(UDF_INIT *initid, char *is_null, char *error);
+void mySum_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
 
 #endif //CS174FINALPROJECT_SUM_H
