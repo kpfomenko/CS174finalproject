e530634790991b9eccf07bbf15f607eef842577e
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-11-30 21:01:02 -0800

    implemented sum functions so aggregate sum should work (not tested)

--- udf/sum.c
+++ udf/sum.c
@@ -1,18 +1,36 @@
-//
-// Created by Deanna Hartsook on 11/29/15.
-//
-
+#include <string.h>
 #include "sum.h"
 
 long long MySum(UDF_INIT *initid, UDF_ARGS *args,
           char *is_null, char *error) {
-    return 10;
+
+    return *((longlong *) initid->ptr);
 }
 
 my_bool MySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+    longlong * sum = (longlong *) malloc(sizeof(longlong));
+    *sum = 0;
+
+    initid->ptr = (char*) sum;
+
+    if (args->arg_count != 1)
+    {
+        strcpy(message,"MyTest() requires one arguments");
+        return 1;
+    }
+
     return 0;
 }
 
 void MySum_deinit(UDF_INIT *initid) {
+    free(initid->ptr);
+}
 
+void MySum_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error) {
+    *((longlong*)initid->ptr) = *((longlong*)initid->ptr) +
+                                *((longlong*)args->args[0]);
+}
+
+void MySum_clear(UDF_INIT *initid, char *is_null, char *error) {
+    *((longlong *) initid->ptr) = 0;
 }
--- udf/sum.h
+++ udf/sum.h
@@ -1,17 +1,22 @@
 //
-// Created by Deanna Hartsook on 11/29/15.
+// Implementation of aggregate sum function.
 //
 
 #ifndef CS174FINALPROJECT_SUM_H
 #define CS174FINALPROJECT_SUM_H
 
 #include <mysql.h>
+#include <my_global.h>
 
-long long MySum(UDF_INIT *initid, UDF_ARGS *args,
+longlong MySum(UDF_INIT *initid, UDF_ARGS *args,
               char *is_null, char *error);
 
 my_bool MySum_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 
 void MySum_deinit(UDF_INIT *initid);
+
+void MySum_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
+
+void MySum_clear(UDF_INIT *initid, char *is_null, char *error);
 
 #endif //CS174FINALPROJECT_SUM_H
