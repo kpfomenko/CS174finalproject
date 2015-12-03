c0777a96013397163c372544d6dc5d7292cefbeb
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-12-02 22:02:33 -0800

    created separate encrypt, decrypt functions

--- encryption/main.c
+++ encryption/main.c
@@ -3,33 +3,73 @@
 #include "gmp.h"
 #include "../libpaillier-0.8/paillier.h"
 
-int main(int argc, char* argv[]) {
-    printf("Hello World\n");
+static const int KEY_LENGTH = 128;
+static const int ENCRYPTION_BYTE_LENGTH = 65536; // 2^16
+static char *const PRIVATE_KEY_BYTES = "54085728d6701953899d4a01bd31d69e";
+static char* PUBLIC_KEY_BYTES = "a810ae51ace032a8b8743a9ef0baa51d";
 
+void createKeys();
+void * encrypt(char *);
+char* decrypt(void*);
+
+
+// TODO: Remove this code from master branch so it is not on AWS
+
+int main(int argc, char* argv[]) {
+    void* encryption = encrypt("Avocado");
+    printf("encryption: %x\n", (unsigned int) encryption);
+
+    char* decryption = decrypt(encryption);
+    printf("decryption: %s\n", decryption);
+
+    return 0;
+}
+
+
+
+void * encrypt(char *plaintextString) {
+    paillier_pubkey_t* publicKey = paillier_pubkey_from_hex(PUBLIC_KEY_BYTES);
+    paillier_plaintext_t* plainText = paillier_plaintext_from_str(plaintextString);
+    paillier_ciphertext_t* ciphertext = NULL;
+
+    ciphertext = paillier_enc(ciphertext, publicKey, plainText, &paillier_get_rand_devurandom);
+    void* ciphertextString = paillier_ciphertext_to_bytes(ENCRYPTION_BYTE_LENGTH, ciphertext);
+
+    paillier_freeplaintext(plainText);
+    paillier_freeciphertext(ciphertext);
+    paillier_freepubkey(publicKey);
+
+    return ciphertextString;
+}
+
+char* decrypt(void* cipherTextBytes) {
+    paillier_ciphertext_t* ciphertext = paillier_ciphertext_from_bytes(cipherTextBytes, ENCRYPTION_BYTE_LENGTH);
+    paillier_pubkey_t* publicKey = paillier_pubkey_from_hex(PUBLIC_KEY_BYTES);
+    paillier_prvkey_t* privateKey = paillier_prvkey_from_hex(PRIVATE_KEY_BYTES, publicKey);
+
+    paillier_plaintext_t* decryptedText = NULL;
+    decryptedText = paillier_dec(decryptedText, publicKey, privateKey, ciphertext);
+    char* decryptedTextString = paillier_plaintext_to_str(decryptedText);
+
+    paillier_freeciphertext(ciphertext);
+    paillier_freepubkey(publicKey);
+    paillier_freeprvkey(privateKey);
+
+    return  decryptedTextString;
+}
+
+void createKeys() {
     paillier_pubkey_t* publicKey = (paillier_pubkey_t*) malloc(sizeof(paillier_pubkey_t));
     paillier_prvkey_t* privateKey = (paillier_prvkey_t*) malloc(sizeof(paillier_prvkey_t));
 
-    paillier_keygen(128, &publicKey, &privateKey, &paillier_get_rand_devurandom);
+    paillier_keygen(KEY_LENGTH, &publicKey, &privateKey, &paillier_get_rand_devurandom);
 
-    paillier_ciphertext_t* ciphertext = paillier_create_enc_zero();
-    paillier_plaintext_t* plainText = paillier_plaintext_from_str("Avocado");
+    char* publicKeyHex = paillier_pubkey_to_hex(publicKey);
+    char* privateKeyHex = paillier_prvkey_to_hex(privateKey);
 
-    paillier_enc(ciphertext, publicKey, plainText, &paillier_get_rand_devurandom);
-
-    printf("public key %s\n", paillier_pubkey_to_hex(publicKey));
-    printf("private key %s\n", paillier_prvkey_to_hex(privateKey));
-    printf("plainText: %s\n", paillier_plaintext_to_str(plainText));
-
-    paillier_plaintext_t*decryptedText = NULL;
-    decryptedText = paillier_dec(decryptedText, publicKey, privateKey, ciphertext);
-    printf("decrypted text: %s\n", paillier_plaintext_to_str(decryptedText));
-
+    printf("public key: %s\n", publicKeyHex);
+    printf("private key: %s\n", privateKeyHex);
 
     paillier_freepubkey(publicKey);
     paillier_freeprvkey(privateKey);
-    paillier_freeciphertext(ciphertext);
-    paillier_freeplaintext(plainText);
-    paillier_freeplaintext(decryptedText);
-
-    return 0;
 }
