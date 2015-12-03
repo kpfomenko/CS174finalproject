b28316031d98a82834a345d3360b71660cb68f47
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-12-02 21:03:39 -0800

    Created and printing public and private keys

--- encryption/main.c
+++ encryption/main.c
@@ -1,14 +1,17 @@
-//
-// Created by Deanna Hartsook on 11/29/15.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 #include "gmp.h"
-#include "paillier.h"
+#include "../libpaillier-0.8/paillier.h"
 
 int main(int argc, char* argv[]) {
     printf("Hello World\n");
 
-    paillier_pubkey_t key;
+    paillier_pubkey_t* publicKey = (paillier_pubkey_t*) malloc(sizeof(paillier_pubkey_t));
+    paillier_prvkey_t* privateKey = (paillier_prvkey_t*) malloc(sizeof(paillier_prvkey_t));
+
+    paillier_keygen(128, &publicKey, &privateKey, &paillier_get_rand_devurandom);
+
+    printf("public key %s\n", paillier_pubkey_to_hex(publicKey));
+    printf("private key %s\n", paillier_prvkey_to_hex(privateKey));
     return 0;
 }
