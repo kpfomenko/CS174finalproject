a6a69dd9f82f6997f398b6d26ef27c4019dd2c17
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-12-03 20:46:22 -0800

    copied in server connection code from project 2 to use as model

--- commandline/commandline.py
+++ commandline/commandline.py
@@ -1,3 +1,46 @@
+from __future__ import print_function
+import sys
+
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'localPass174', 
+  'host': '127.0.0.1', # Localhost. If your MySQL Server is running on your own computer.
+  'port': '3306', # Default port on Windows/Linux is 3306. On Mac it may be 3307.
+  'database': 'airport',
+}
+
+try:
+  cnx = mysql.connector.connect(**config)
+  cursor = cnx.cursor()
+except mysql.connector.Error as err:
+  print("Connection Error: {}".format(err))
+  sys.exit(1)
+
+def execute(query, values):
+  your_query = query % values
+  print("Executing: {} ... ".format(query % values), end="")
+  try:
+    cursor.execute(query, values)
+  except mysql.connector.Error as err:
+    print("ERROR\nMySQL Error: {}\n".format(err))
+    sys.exit(1)
+  else:
+    print("Success")
+
+
+if __name__ == '__main__':
+  # Commit data
+  cnx.commit()
+
+  cursor.close()
+  cnx.close()
+
+
+
+
+
 
 # Functions:
 def createSelectQuery(tokenList):
