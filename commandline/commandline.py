d17577c010adade9b733385bf4a012843e8bffb4
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2015-12-05 19:05:20 -0800

    implemented select id

--- commandline/commandline.py
+++ commandline/commandline.py
@@ -5,7 +5,8 @@ import subprocess
 
 config = {
 	'user': 'root',
-	'password': 'password1',
+	# 'password': 'password1',
+	'password': 'localPass174',
 	'host': '127.0.0.1', # Localhost. If your MySQL Server is running on your own computer.
 	'port': '3306', # Default port on Windows/Linux is 3306. On Mac it may be 3307.
 	'database': 'project',
@@ -22,6 +23,7 @@ columnList = ["emp_id", "emp_age", "emp_salary"]
 
 def execute(query, values):
 	your_query = query % values
+	print("\n")
 	print("Executing: {} ... ".format(query % values), end="")
 	try:
 		cursor.execute(query, values)
@@ -32,12 +34,10 @@ def execute(query, values):
 		print("Success")
 	print("----------------------------------------------------------\n")
 
-def createSelectAllQuery():
-	sql_query = "SELECT * FROM Employees"
-	execute(sql_query, {})
+def printQueryResults():
 	rows = cursor.fetchall()
 	if not rows:
-		print("There are no employees in the database.\n")
+		print("No employees were found.\n")
 	else:
 		row_format = "{:10s} {:10s} {:100s}"
 		print(row_format.format(*columnList))
@@ -46,16 +46,24 @@ def createSelectAllQuery():
 		for attributes in rows:
 			print(row_format.format(*attributes))
 
+def createSelectAllQuery():
+	sql_query = "SELECT * FROM Employees"
+	execute(sql_query, {})
+	printQueryResults()
+
+def createSelectEmployee(id):
+	sql_query = "SELECT * FROM Employees WHERE id = %(emp_id)s"
+	execute(sql_query, {"emp_id": id})
+	printQueryResults()
+
+
 def createSelectQuery(tokenList):
 	if (tokenList[0] == "*"):
 		createSelectAllQuery()
+	elif (tokenList[0] == "SUM"):
+			printf("This Option is not yet implemented.")
 	else:
-		printf("This Option is not yet implemented.")
-		
-	# SELECT salary FROM Employees
-	# sql_query = "SELECT salary FROM Employees WHERE id = 112"
-	# sql_values = {}
-	# execute(sql_query, sql_values)	
+		createSelectEmployee(tokenList[0])
 
 	# for salary in cursor:
 	# 	# print(type(salary[0]))
@@ -80,21 +88,21 @@ def createInsertQuery(tokenList):
 	execute(sql_query, sql_values)
 	
 if __name__ == '__main__':
-	statement = raw_input('Please enter your query: \n')
-	# print 'Your statement: %s' % statement
+	while(True):
+		statement = raw_input('Please enter your query: \n')
 
-	# Parsing:
-	tokenList = statement.split(" ")
-	# print 'Tokens: '
-	# print tokenList
-	action = tokenList[0].upper()
-	if action == 'SELECT':
-		createSelectQuery(tokenList[1:])
-	elif action == 'INSERT':
-		createInsertQuery(tokenList[1:])
-		cnx.commit()
-	else:
-		print ('Error: invalid action -- %s' % tokenList[0])
+		# Parsing:
+		tokenList = statement.split(" ")
+		action = tokenList[0].upper()
+		if action == 'SELECT':
+			createSelectQuery(tokenList[1:])
+		elif action == 'INSERT':
+			createInsertQuery(tokenList[1:])
+			cnx.commit()
+		elif action == 'EXIT':
+			break
+		else:
+			print ('Error: invalid query -- %s' % statement)
 
 	cursor.close()
 	cnx.close()
